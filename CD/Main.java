520c0daac97574e3d1d1b47003ce355876501be8
fyrvelm <fyrvelm@gmail.com>
2016-11-07 19:55:32 +0100

    CD problem
    https://open.kattis.com/problems/cd

---
+++ CD/Main.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
+public class Main {
+
+	private static HashMap<Integer, Boolean> ownedCDs;
+	private static int Jack, Jill;
+
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+		boolean EOF = false;
+		while(!EOF){
+			if(getTotCDs(stdin) == 0)
+			{
+				EOF = true;
+			}
+			else{
+				ownedCDs = new HashMap<Integer, Boolean>(Jack+Jill);
+				System.out.println(readList(stdin));
+			}
+		}
+	}
+
+
+	private static int readList(BufferedReader stdin) {
+		int clashes = 0;
+		int curr;
+		try{
+			for(int i = 0; i < Jack + Jill; ++i){
+				curr = Integer.parseInt(stdin.readLine());
+				if(ownedCDs.containsKey(curr))
+					clashes++;
+				else
+					ownedCDs.put(curr, true);
+			}
+		}
+		catch(IOException e)
+		{
+
+		}
+		return clashes;
+	}
+
+	/**
+	 * @param stdin
+	 * @return sum of cds owned by Jack and Jill
+	 */
+	private static int getTotCDs(BufferedReader stdin) {
+		int sum = 0;
+		try {
+			//problem assignment tells us that there will be exactly 2 numbers on line 0 + n.
+			String[] s = stdin.readLine().split(" ");
+			Jack = Integer.parseInt(s[0]);
+			Jill = Integer.parseInt(s[1]);
+			sum = Jack + Jill;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return sum;
+	}
+}
---
+++ CD/README.md
@@ -0,0 +1,10 @@
+CD
+Jack and Jill have decided to sell some of their Compact Discs, while they still have some value. They have decided to sell one of each of the CD titles that they both own. How many CDs can Jack and Jill sell?
+
+Neither Jack nor Jill owns more than one copy of each CD.
+
+Input
+The input consists of a sequence of test cases. The first line of each test case contains two non-negative integers NN and MM, each at most one million, specifying the number of CDs owned by Jack and by Jill, respectively. This line is followed by NN lines listing the catalog numbers of the CDs owned by Jack in increasing order, and MM more lines listing the catalog numbers of the CDs owned by Jill in increasing order. Each catalog number is a positive integer no greater than one billion. The input is terminated by a line containing two zeros. This last line is not a test case and should not be processed.
+
+Output
+For each test case, output a line containing one integer, the number of CDs that Jack and Jill both own.
