955739b0c38847a22daabe05b2e65e5b37d07478
csundlof <claudiussundlof@gmail.com>
2016-11-26 21:04:04 +0100

    indentation

--- modulo/modulo.cpp
+++ modulo/modulo.cpp
@@ -4,9 +4,8 @@
 int main(void) {
 	std::bitset<42> nums;
 	int a;
-    while(std:: cin >> a)
-    	nums.set(a%42);
-    std::cout << nums.count();
-    while(true);
-    return 0;   
+	while(std:: cin >> a)
+		nums.set(a%42);
+	std::cout << nums.count();
+	return 0;   
 }
