a2ecf56f514bc598627e2e1653ac6f2915898fd1
Claudius <fyrvelm@gmail.com>
2017-09-13 18:46:02 +0300

    Update pauleigon.cpp

--- pauleigon/pauleigon.cpp
+++ pauleigon/pauleigon.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#include <string.h>
 
 int main(void) {
 	int n, p, q;
 	while(std:: cin >> n >> p >> q)
 	{
-		int rounds = 0;
-		rounds += (p+q)/n;
-		std::cout << (rots % 2 == 0 ? "paul" : "opponent");
+		std::cout << ((p+q)/n % 2 == 0 ? "paul" : "opponent");
 	}
 	return 0;   
 }
