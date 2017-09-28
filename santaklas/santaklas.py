c900afb8be1b8d7d931a895f097891d4a9aad5be
csundlof <claudiussundlof@gmail.com>
2017-09-28 23:15:39 +0200

    santaklas

--- santaklas/santaklas.py
+++ santaklas/santaklas.py
@@ -5,5 +5,4 @@ v = int(x[1])
 if(v <= 180):
 	print("safe")
 else:
-	print "%i" % math.floor(abs(int(x[0]) / math.sin(math.radians(v))))
-	print("%i" %(math.floor(abs(int(x[0]) / math.sin(math.radians(v))))))
+	print(int(math.floor(abs(int(x[0]) / math.sin(math.radians(v))))))
