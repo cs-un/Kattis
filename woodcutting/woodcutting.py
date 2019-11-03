dca5f3e7c7072aa439fca46d551f6096a6a81edd
csundlof <claudiussundlof@gmail.com>
2019-11-03 01:53:09 +0000

    woodcutting

---
+++ woodcutting/woodcutting.py
@@ -0,0 +1,14 @@
+import sys
+
+_ = sys.stdin.readline()
+
+for customers in sys.stdin:
+   waits = [0]
+   for _ in range(int(customers)):
+      input = list(map(int, sys.stdin.readline().split()))
+      waits.append(input[0] * sum(input[1:])/len(input[1:]))
+   waits.sort()
+   print(len(waits))
+   for i in range(1, len(waits)):
+      waits[i] = waits[i - 1] + waits[i]
+   print(sum(waits)/(len(waits) - 1))
